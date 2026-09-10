5f0e9777422773076b8cbcfe90cb41c33d7c3e69
tom38121605 <13538121605@139.com>
2026-09-10 12:45:19 +0800

    111

---
+++ 示例_对测/ti_对测_spi主从/对测01-主机发送一个字节-从机返回-ok/22-02B-sendsting-slaveMO-PB8/empty.c
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2021, Texas Instruments Incorporated
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * *  Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * *  Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * *  Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+  
+#include "ti_msp_dl_config.h"
+
+#include "stdio.h"
+#include "string.h"
+
+
+void SendString(char *str);
+char txBuf[100];
+
+volatile uint8_t gEchoData = 0;
+volatile uint8_t flg_spi=0;
+
+void delay_ms(uint32_t ms)
+{
+    while(ms--)
+    {
+        delay_cycles(CPUCLK_FREQ/1000);
+    }
+}
+
+
+
+ volatile uint8_t gData;
+
+//int main(void)
+//{
+//    SYSCFG_DL_init();
+
+//    NVIC_ClearPendingIRQ(SPI_0_INST_INT_IRQN);
+//    NVIC_EnableIRQ(SPI_0_INST_INT_IRQN);
+//    gData = 1;
+
+//    while (1) 
+//    {
+//        /* Wait for interrupt until a push button is pressed */
+//        __WFI();
+//    }
+//}
+
+int main(void)
+{
+    SYSCFG_DL_init();
+
+    NVIC_ClearPendingIRQ(SPI_1_INST_INT_IRQN);
+    NVIC_EnableIRQ(SPI_1_INST_INT_IRQN);
+    gData = 1;
+
+    while (1) 
+    {
+        if(flg_spi==1)
+        {
+           flg_spi=0;
+           sprintf(txBuf, "echo: %d\r\n", gData);
+           SendString(txBuf);  
+        }        
+        
+    }
+}
+
+void SPI_1_INST_IRQHandler(void)
+{
+    switch (DL_SPI_getPendingInterrupt(SPI_1_INST)) {
+        case DL_SPI_IIDX_RX:
+
+            gData = DL_SPI_receiveData8(SPI_1_INST);
+            DL_SPI_transmitData8(SPI_1_INST, gData);
+
+            flg_spi=1;
+
+            break;
+        default:
+            break;
+    }
+}
+
+
+
+
+
+void UART_0_INST_IRQHandler(void)
+{
+    switch (DL_UART_Main_getPendingInterrupt(UART_0_INST)) 
+    {
+        case DL_UART_MAIN_IIDX_RX:
+
+            gEchoData = DL_UART_Main_receiveData(UART_0_INST);
+            DL_UART_Main_transmitData(UART_0_INST, gEchoData);
+            break;
+        default:
+            break;
+    }
+}
+
+
+void SendString(char *str)
+{
+    while(*str != '\0')
+    {
+        DL_UART_Main_transmitDataBlocking(UART_0_INST, *str++);
+    }
+}
